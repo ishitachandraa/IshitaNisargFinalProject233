136b89a498964bc3537ae70b90271233375bb010
nisarg.bhalani <nisarg.bhalani@IC-CS233-41.uc.ucalgary.ca>
2022-08-06 10:12:31 -0600

    Created controller and a new fxml file.

---
+++ src/application/BabyCalController.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class BabyCalController {
+	Stage applicationStage;
+	
+    @FXML
+    private ChoiceBox<?> motherEarLobeChoiceBox;
+
+    @FXML
+    private ChoiceBox<?> motherHairColorChoiceBox;
+
+    @FXML
+    private TextField motherHeightTextField;
+
+    @FXML
+    private Button finalCalculate;
+
+    @FXML
+    private ChoiceBox<?> eyeColorFatherChoiceBox;
+
+    @FXML
+    private ChoiceBox<?> fatherEarLobeChoiceBox;
+
+    @FXML
+    private ChoiceBox<?> fatherDimplesChoiceBox;
+
+    @FXML
+    private ChoiceBox<?> motherDimplesChoiceBox;
+
+    @FXML
+    private ChoiceBox<?> fatherHairColorChoiceBox;
+
+    @FXML
+    private TextField fatherHeightTextField;
+
+    @FXML
+    private ChoiceBox<?> eyeColorMotherChoiceBox;
+
+    
+    @FXML
+    void calculateBaby(ActionEvent event)throws IOException {
+    	String motherHeight = motherHeightTextField.getText();
+    	String fatherHeight = fatherHeightTextField.getText();
+    	
+    	FXMLLoader loader = new FXMLLoader();
+    	VBox root = loader.load(new FileInputStream("src/application/BabyFeatures.fxml"));
+    	
+    	Scene any = new Scene(root,600,550);
+    	applicationStage.setScene(any);
+    }
+
+}
