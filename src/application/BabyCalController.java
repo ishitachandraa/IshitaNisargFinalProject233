9c5c7ddd2e737c456c8626ccd16f442f8a8f5a31
nisarg.bhalani <nisarg.bhalani@IC-CS233-44.uc.ucalgary.ca>
2022-08-14 18:39:17 -0600

    Made Height.java and updated controller.

--- src/application/BabyCalController.java
+++ src/application/BabyCalController.java
@@ -84,6 +84,10 @@ public class BabyCalController {
     	String fatherDimples = fatherDimplesChoiceBox.getValue().toString();
     	String babyDim = calculateDimples(motherDimples, fatherDimples);
     	
+    	//Height
+    	float motherHeight = Integer.parseInt(motherHeightTextField.getText());
+    	float fatherHeight = Integer.parseInt(fatherHeightTextField.getText());
+    	float babyHeight = calculateHeight(motherHeight, fatherHeight);
     
     }
     
@@ -114,5 +118,11 @@ public class BabyCalController {
 	   String dimResult = babyDim.predictDimples();
 	   return dimResult;
    }
-    
+   
+   float calculateHeight(float mHeight, float fHeight) {
+	   
+	   Height babyHeight = new Height(mHeight, fHeight);
+	   float heightResult = babyHeight.predictHeight();
+	   return heightResult;
+   }
 }
