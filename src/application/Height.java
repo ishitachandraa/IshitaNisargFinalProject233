2f8d404e838fbf6821260a0eb1f37e4ba0f0bcc2
ishita.chandra <ishita.chandra@IC-CS233-9.uc.ucalgary.ca>
2022-08-17 06:29:29 +0530

    Validation complete

--- src/application/Height.java
+++ src/application/Height.java
@@ -1,48 +1,59 @@
 
 package application;
 
+import java.util.ArrayList;
+
 public class Height {
-String mHeight;
-String fHeight;
+	String mHeight;
+	String fHeight;
+    ArrayList<Character> badChar = new ArrayList<Character>();
 
 
+	Height(String motherHeight, String fatherHeight){
+		mHeight = motherHeight;
+		fHeight = fatherHeight;
+	}
+	String predictHeight() {
+		String bHeight = "";
+		boolean valueOkay = true;
 
-Height(String motherHeight, String fatherHeight){
-mHeight = motherHeight;
-fHeight = fatherHeight;
-}
-String predictHeight() {
-String bHeight = "";
-boolean valueOkay = true;
+		char[] mArray = mHeight.toCharArray();
+		for(char m:mArray) {
+			if (!Character.isDigit(m)) {
+				valueOkay=false;
+				badChar.add(m);
+			}
+		}
 
-char[] mArray = mHeight.toCharArray();
-for(char m:mArray) {
-if (!Character.isDigit(m)) {
-valueOkay=false;
-}
-}
+		char[] fArray = fHeight.toCharArray();
+		for(char f:fArray) {
+			if (!Character.isDigit(f)) {
+				valueOkay=false;
+				badChar.add(f);
+			}
+		}
 
-char[] fArray = fHeight.toCharArray();
-for(char f:fArray) {
-if (!Character.isDigit(f)) {
-valueOkay=false;
-}
-}
-
-if (valueOkay){
-int mInt = Integer.parseInt(mHeight);
-int fInt = Integer.parseInt(fHeight);
-int bInt = (mInt + fInt)/2;
-bHeight = String.valueOf(bInt);
-}
-else {
-bHeight = "0";
-}
-
-bHeight = bHeight+" cm";
-return bHeight;
+		if (valueOkay){
+			int mInt = Integer.parseInt(mHeight);
+			int fInt = Integer.parseInt(fHeight);
+			int bInt = (mInt + fInt)/2;
+			bHeight = String.valueOf(bInt);
+		}
+		else {
+			bHeight = "0";
+		}
+		bHeight = bHeight+" cm";
+		return bHeight;
 
 }
+	
+	String errorMessage() {
+		String message = "";
+		if (badChar.size()!=0) {
+			message = "Only put valid numbers in the height field. '" + badChar.get(0).toString() + "' invalid.";
+		}
+		return message;
+	}
 
 }
 
